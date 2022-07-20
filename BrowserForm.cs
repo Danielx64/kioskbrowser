a0e5c07af5ca62da034bdcf247b2e8e632757a4f
Danielx64 <289220+Danielx64@users.noreply.github.com>
2022-07-20 19:49:57 +1000

    Allow & and = to be in URLs

--- BrowserForm.cs
+++ BrowserForm.cs
@@ -137,7 +137,7 @@ namespace WebView2WindowsFormsBrowser
 		public static string RemoveSpecialChars(string str)
 		{
 			// Create  a string array and add the special characters you want to remove
-			string[] chars = new string[] { "kioskbrowser", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "}", "{", "]", "[", "|", "\"", ":", "'", ":", "?", ">", "<", "/", ".", ",","\\"};
+			string[] chars = new string[] { "kioskbrowser", "~", "`", "!", "@", "#", "$", "%", "^", "*", "(", ")", "_", "+", "}", "{", "]", "[", "|", "\"", ":", "'", ":", "?", ">", "<", "/", ".", ",","\\"};
 
 			//Iterate the number of times based on the String array length.
 			for (int i = 0; i < chars.Length; i++)
