1d52ddd4c6a36144b69ac2d86a97fbd6a0151f64
Danielx64 <289220+Danielx64@users.noreply.github.com>
2022-07-23 16:08:36 +1000

    Change title bar to dark theme

--- BrowserForm.cs
+++ BrowserForm.cs
@@ -9,16 +9,17 @@ using Microsoft.Web.WebView2.Core;
 using System.IO;
 using Microsoft.Web.WebView2.WinForms;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace WebView2WindowsFormsBrowser
 {
 	public partial class BrowserForm : Form
 	{
-
 		public BrowserForm()
 		{
 					InitializeComponent();
-					InitializeBrowser();
+			UseImmersiveDarkMode(this.Handle, true);
+			InitializeBrowser();
 					AttachControlEventHandlers();
 					HandleResize();
 		}
@@ -149,6 +150,38 @@ namespace WebView2WindowsFormsBrowser
 			}
 			str = str.Replace($"{Globals.URI_SCHEMA}", "");
 			return str;
+		}
+
+		/*
+using System.Runtime.InteropServices;
+*/
+
+		[DllImport("dwmapi.dll")]
+		private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+		private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+		private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+		private static bool UseImmersiveDarkMode(IntPtr handle, bool enabled)
+		{
+			if (IsWindows10OrGreater(17763))
+			{
+				var attribute = DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1;
+				if (IsWindows10OrGreater(18985))
+				{
+					attribute = DWMWA_USE_IMMERSIVE_DARK_MODE;
+				}
+
+				int useImmersiveDarkMode = enabled ? 1 : 0;
+				return DwmSetWindowAttribute(handle, (int)attribute, ref useImmersiveDarkMode, sizeof(int)) == 0;
+			}
+
+			return false;
+		}
+
+		private static bool IsWindows10OrGreater(int build = -1)
+		{
+			return Environment.OSVersion.Version.Major >= 10 && Environment.OSVersion.Version.Build >= build;
 		}
 	}
 }
